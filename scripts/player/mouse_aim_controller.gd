ff61b1b8791d23b816c469cc81936eb3fd32560d
Jeli <105231781+Jeliciousz@users.noreply.github.com>
2025-04-26 00:36:13 -0400

    Finalize MouseAimController

--- scripts/player/mouse_aim_controller.gd
+++ scripts/player/mouse_aim_controller.gd
@@ -1,14 +1,15 @@
-extends Node
+class_name MouseAimController extends Node
+## Rotates a body and a head for FPS camera control with the mouse.
+##
+## Code courtesy of [url=https://yosoyfreeman.github.io/article/godot/tutorial/achieving-better-mouse-input-in-godot-4-the-perfect-camera-controller/]Yo Soy Freeman[/url]. Edited by Jeliciousz
 
+const RADIANS_PER_UNIT: float = deg_to_rad(0.2)
 
-# Code courtesy of Yo Soy Freeman | https://yosoyfreeman.github.io/article/godot/tutorial/achieving-better-mouse-input-in-godot-4-the-perfect-camera-controller/
-# Slightly edited by Jeliciousz
-
-
+## Node references.
 @export_group("Nodes")
 
-## The Player.
-@export var player: Player
+## The body.
+@export var body: Node3D
 
 ## The head.
 @export var head: Node3D
@@ -20,42 +21,23 @@ extends Node
 ## Mouse settings.
 @export_subgroup("Mouse")
 
-@export_range(0.01, 1.00, 0.01, "or_greater") var mouse_sensitivity: float = 0.5
-
+## How sensitive aiming is.
+@export var sensitivity: Vector2 = Vector2(0.5, 0.5)
 
 ## Camera pitch clamping.
 @export_subgroup("Clamping")
 
 ## Maximum camera pitch in radians.
-@export_range(0, 89, 1, "radians_as_degrees") var max_pitch: float = deg_to_rad(89.5)
+@export_range(0, 90, 0.5, "radians_as_degrees") var max_pitch: float = deg_to_rad(89.5)
 
 ## Minimum camera pitch in radians.
-@export_range(-89, 0, 1, "radians_as_degrees") var min_pitch: float = deg_to_rad(-89.5)
+@export_range(-90, 0, 0.5, "radians_as_degrees") var min_pitch: float = deg_to_rad(-89.5)
 
 
-func _ready() -> void:
-	Input.set_use_accumulated_input(false)
-
-
-func _unhandled_input(event: InputEvent)-> void:
+func _unhandled_input(event: InputEvent) -> void:
 	if Input.mouse_mode != Input.MOUSE_MODE_CAPTURED:
-		if event is InputEventKey:
-			if event.is_action_pressed("ui_cancel"):
-				get_tree().quit()
-				return
-		
-		if event is InputEventMouseButton:
-			if event.button_index == 1:
-				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-		
 		return
-	
-	if event is InputEventKey:
-		if event.is_action_pressed("ui_cancel"):
-			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		
-		return
-	
+
 	if event is InputEventMouseMotion:
 		aim(event)
 
@@ -64,11 +46,10 @@ func _unhandled_input(event: InputEvent)-> void:
 func aim(event: InputEventMouseMotion) -> void:
 	var viewport_transform: Transform2D = get_tree().root.get_final_transform()
 	var motion: Vector2 = event.xformed_by(viewport_transform).relative
-	var radians_per_unit: float = deg_to_rad(0.2)
-	
-	motion *= radians_per_unit
-	motion *= mouse_sensitivity
-	
+
+	motion *= RADIANS_PER_UNIT
+	motion *= sensitivity
+
 	add_yaw(motion.x)
 	add_pitch(motion.y)
 	clamp_pitch()
@@ -78,16 +59,16 @@ func aim(event: InputEventMouseMotion) -> void:
 func add_yaw(amount: float) -> void:
 	if is_zero_approx(amount):
 		return
-	
-	player.rotate_object_local(Vector3.DOWN, amount)
-	player.orthonormalize()
+
+	body.rotate_object_local(Vector3.DOWN, amount)
+	body.orthonormalize()
 
 
 ## Rotates the head around the local x axis by a given amount (in radians) to achieve pitch.
 func add_pitch(amount: float) -> void:
 	if is_zero_approx(amount):
 		return
-	
+
 	head.rotate_object_local(Vector3.LEFT, amount)
 	head.orthonormalize()
 
@@ -96,6 +77,6 @@ func add_pitch(amount: float) -> void:
 func clamp_pitch() -> void:
 	if head.rotation.x > min_pitch and head.rotation.x < max_pitch:
 		return
-	
+
 	head.rotation.x = clamp(head.rotation.x, min_pitch, max_pitch)
 	head.orthonormalize()
