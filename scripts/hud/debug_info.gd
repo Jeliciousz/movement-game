f780bdf2fc4b85152497b8edee78aef46db72a65
Jeli <105231781+Jeliciousz@users.noreply.github.com>
2025-04-26 02:57:30 -0400

    Clean up whitespace

--- scripts/hud/debug_info.gd
+++ scripts/hud/debug_info.gd
@@ -6,7 +6,7 @@ extends Label
 
 func _process(_delta: float) -> void:
 	var stance: String
-	
+
 	match player.active_stance:
 		player.Stances.STANDING:
 			stance = "Standing"
@@ -14,5 +14,5 @@ func _process(_delta: float) -> void:
 			stance = "Crouching"
 		player.Stances.SPRINTING:
 			stance = "Sprinting"
-	
+
 	text = "Active state: %s\nActive stance: %s\nCoyote jump active: %s\nCoyote slide active: %s\nCoyote wall-jump active: %s" % [player.state_machine.active_state_name, stance, player.coyote_jump_active, player.coyote_slide_active, player.coyote_walljump_active]
