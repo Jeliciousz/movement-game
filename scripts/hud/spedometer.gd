09a640054a655b1a428253769893d3640ce53cb9
Jeli <105231781+Jeliciousz@users.noreply.github.com>
2025-04-26 02:58:04 -0400

    Add smoothing from the speed shown on the label to the player's actual speed

--- scripts/hud/spedometer.gd
+++ scripts/hud/spedometer.gd
@@ -4,6 +4,12 @@ extends Label
 @export var player: Player
 
 
-func _process(_delta: float) -> void:
+var label_speed: float = 0
+
+
+func _process(delta: float) -> void:
 	var horizontal_speed: float = Vector2(player.velocity.x, player.velocity.z).length()
-	text = "%.fkph" % (horizontal_speed * 3.6)
+
+	label_speed = lerpf(label_speed, horizontal_speed, 15 * delta)
+
+	text = "%.fkph" % (label_speed * 3.6)
