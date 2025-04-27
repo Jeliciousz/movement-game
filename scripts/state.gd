191a47fd846c2328b8ae7a6c22c68f1a373931f0
Jeli <105231781+Jeliciousz@users.noreply.github.com>
2025-04-27 00:39:06 -0400

    Remake state machine

---
+++ scripts/state.gd
@@ -0,0 +1,41 @@
+class_name State
+extends Node
+## A state used by a [StateMachine].
+##
+## Can be entered, exited, and updated per physics and idle frame.
+
+## The function a state can call to transition to a new state.
+var change_state_to: Callable
+
+## The dictionary of values shared between the states in a state machine.
+var shared_vars: Dictionary[StringName, Variant]
+
+
+## Called when the state is transitioned to.
+func _state_enter() -> void:
+	pass
+
+
+## Called on process.
+func _state_process(_delta: float) -> void:
+	pass
+
+
+## Called on physics process.
+func _state_physics_process(_delta: float) -> void:
+	pass
+
+
+## Called on input.
+func _state_input(_event: InputEvent) -> void:
+	pass
+
+
+## Called on unhandled input.
+func _state_unhandled_input(_event: InputEvent) -> void:
+	pass
+
+
+## Called when the state is transitioned from.
+func _state_exit() -> void:
+	pass
---
+++ scripts/state_machine.gd
@@ -0,0 +1,77 @@
+class_name StateMachine
+extends Node
+## Handles updating and switching between [State]s.
+
+## Emitted when the [StateMachine] changes state.
+signal state_changed(last: StringName, current: StringName)
+
+## The [State] that this [StateMachine] will start in.
+@export var initial_state: State
+
+## The dictionary of values shared between the states in a state machine.
+var shared_vars: Dictionary[StringName, Variant] = {}
+
+## The currently active state.
+@onready var _state: State = initial_state
+
+
+func _ready() -> void:
+	if _state == null:
+		return
+
+	_state.change_state_to = change_state_to
+	_state.shared_vars = shared_vars
+	_state._state_enter()
+
+
+func _process(delta: float) -> void:
+	if _state == null:
+		return
+
+	_state._state_process(delta)
+
+
+func _physics_process(delta: float) -> void:
+	if _state == null:
+		return
+
+	_state._state_physics_process(delta)
+
+
+func _input(event: InputEvent) -> void:
+	if _state == null:
+		return
+
+	_state._state_input(event)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if _state == null:
+		return
+
+	_state._state_unhandled_input(event)
+
+
+## Returns the name of the current state.
+func get_state_name() -> StringName:
+	return _state.name
+
+
+## Transition to a new state.
+func change_state_to(state: StringName) -> void:
+	if not has_node(NodePath(state)):
+		printerr("The StateMachine does not have a child State with the name: '%s'" % state)
+		return
+
+	var last_state_name: StringName = ""
+
+	if _state != null:
+		last_state_name = _state.name
+		_state._state_exit()
+
+	_state = get_node(NodePath(state))
+	_state.change_state_to = change_state_to
+	_state.shared_vars = shared_vars
+	_state._state_enter()
+
+	state_changed.emit(last_state_name, _state.name)
