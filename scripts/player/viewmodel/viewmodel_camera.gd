4ae72a2f81a02a8ec29501ecbbc26f5a44475cdb
Jeli <105231781+Jeliciousz@users.noreply.github.com>
2025-04-27 00:49:24 -0400

    Make viewmodel_camera follow style guides

--- scripts/player/viewmodel/viewmodel_camera.gd
+++ scripts/player/viewmodel/viewmodel_camera.gd
@@ -1,9 +1,8 @@
 extends Camera3D
 
-
 ## The world camera to transform the viewmodel camera to.
-@export var world_camera: Camera3D
+@export var _world_camera: Camera3D
 
 
 func _physics_process(_delta: float) -> void:
-	global_transform = world_camera.global_transform
+	global_transform = _world_camera.global_transform
