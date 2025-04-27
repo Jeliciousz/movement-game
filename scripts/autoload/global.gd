588b4fa962ffc5b41ec1ddcceb8cac5e0c0361fc
Jeli <105231781+Jeliciousz@users.noreply.github.com>
2025-04-27 06:07:54 -0400

    Update Global

--- scripts/autoload/global.gd
+++ scripts/autoload/global.gd
@@ -7,4 +7,18 @@ const MIN_INT: int = -9223372036854775808
 
 func _init() -> void:
 	Input.use_accumulated_input = false
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.is_action(&"ui_cancel") and event.pressed and not event.echo:
+			if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+				Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			else:
+				get_tree().quit()
+
+	elif event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			if Input.mouse_mode != Input.MOUSE_MODE_CAPTURED:
+				Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
