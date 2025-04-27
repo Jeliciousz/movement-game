c4c3664f5a769f4e79907883656f36cba7e09711
Jeli <105231781+Jeliciousz@users.noreply.github.com>
2025-04-27 00:48:34 -0400

    Make grapple_hook_point follow style guides

--- scripts/grapple_hook_point.gd
+++ scripts/grapple_hook_point.gd
@@ -1,27 +1,32 @@
-class_name GrappleHookPoint extends Area3D
+class_name GrappleHookPoint
+extends Area3D
+## A point the [Player] can fire a grappling hook to.
 
+enum Target {
+	## The player is not looking at the grapple hook point.
+	NOT_TARGETED,
+	## The player is looking at, and is in range of the grapple hook point.
+	TARGETED,
+	## The player is looking at and is out of range of the grapple hook point.
+	INVALID_TARGET,
+}
 
-@onready var collision_shape: CollisionShape3D = $CollisionShape
+var targeted: Target = Target.NOT_TARGETED
 
-@onready var targeted_sprite: Sprite3D = $TargetedSprite
-
-@onready var invalid_target_sprite: Sprite3D = $InvalidTargetSprite
-
-
-enum {NOT_TARGETED, TARGETED, INVALID_TARGET}
-
-
-var targeted: int = NOT_TARGETED
+@onready var _targeted_sprite: Sprite3D = $TargetedSprite
+@onready var _invalid_target_sprite: Sprite3D = $InvalidTargetSprite
 
 
 func _process(_delta: float) -> void:
 	match targeted:
-		NOT_TARGETED:
-			targeted_sprite.hide()
-			invalid_target_sprite.hide()
-		TARGETED:
-			targeted_sprite.show()
-			invalid_target_sprite.hide()
-		INVALID_TARGET:
-			targeted_sprite.hide()
-			invalid_target_sprite.show()
+		Target.NOT_TARGETED:
+			_targeted_sprite.hide()
+			_invalid_target_sprite.hide()
+
+		Target.TARGETED:
+			_targeted_sprite.show()
+			_invalid_target_sprite.hide()
+
+		Target.INVALID_TARGET:
+			_targeted_sprite.hide()
+			_invalid_target_sprite.show()
