c431c473cba0caed9a2063f73989564ee2a21744
Jeli <105231781+Jeliciousz@users.noreply.github.com>
2025-04-27 01:16:54 -0400

    Update debug_info

--- scripts/hud/debug_info.gd
+++ scripts/hud/debug_info.gd
@@ -3,9 +3,6 @@ extends Label
 ## The [Player].
 @export var _player: Player
 
-## The player's [StateMachine].
-@export var _state_machine: StateMachine
-
 
 func _process(_delta: float) -> void:
-	text = "Active state: %s\nActive stance: %s\nCoyote jump active: %s\nCoyote slide active: %s\nCoyote wall-jump active: %s" % [_state_machine.get_state_name(), _player.get_stance_as_text(), _state_machine.shared_vars[&"coyote_jump_active"], _state_machine.shared_vars[&"coyote_slide_active"], _state_machine.shared_vars[&"coyote_walljump_active"]]
+	text = "Active state: %s\nActive stance: %s\nCoyote jump active: %s\nCoyote slide active: %s\nCoyote wall-jump active: %s" % [_player.state_machine.get_state_name(), _player.get_stance_as_text(), _player.state_machine.shared_vars[&"coyote_jump_active"], _player.state_machine.shared_vars[&"coyote_slide_active"], _player.state_machine.shared_vars[&"coyote_walljump_active"]]
