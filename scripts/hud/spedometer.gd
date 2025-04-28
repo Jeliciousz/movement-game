f90f9deab14d46aaeb181679ae60a60257c32fdb
Jeli <105231781+Jeliciousz@users.noreply.github.com>
2025-04-27 23:10:58 -0400

    Update spedometer

--- scripts/hud/spedometer.gd
+++ scripts/hud/spedometer.gd
@@ -6,12 +6,13 @@ extends Label
 ## How quickly the speed shown on the spedometeter updates to match the player's speed.
 @export var _speed_track_rate: float = 100.0
 
-var _label_speed_kph: float = 0.0
+var _label_speed: float = 0.0
 
 
 func _process(delta: float) -> void:
-	var horizontal_speed_kph: float = Vector2(_player.velocity.x, _player.velocity.z).length() * 3.6
+	# Kilometers Per Hour
+	var horizontal_speed: float = Vector2(_player.velocity.x, _player.velocity.z).length() * 3.6
 
-	_label_speed_kph = move_toward(_label_speed_kph, horizontal_speed_kph, _speed_track_rate * delta)
+	_label_speed = move_toward(_label_speed, horizontal_speed, _speed_track_rate * delta)
 
-	text = "%.f kph" % _label_speed_kph
+	text = "%.f kph" % _label_speed
