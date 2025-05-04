05c2c139296e482df4e9d80206c8998dbc55bfa7
Jeli <105231781+Jeliciousz@users.noreply.github.com>
2025-05-04 17:23:41 -0400

    New death area

---
+++ scripts/death_area.gd
@@ -0,0 +1,14 @@
+class_name DeathArea
+extends Area3D
+## If a [Player] enters this area, they will die.
+
+
+func _physics_process(_delta: float) -> void:
+	if not has_overlapping_bodies():
+		return
+
+	for player: Player in get_overlapping_bodies():
+		if not player is Player:
+			continue
+
+		player.state_machine.change_state_to(&"Spawning")
