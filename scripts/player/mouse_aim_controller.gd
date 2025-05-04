0d64224dc3a07881ddbbf1c0d4a69a0e549916c4
Jeli <105231781+Jeliciousz@users.noreply.github.com>
2025-05-04 16:36:19 -0400

    Increase radians per unit

--- scripts/player/mouse_aim_controller.gd
+++ scripts/player/mouse_aim_controller.gd
@@ -4,7 +4,7 @@ extends Node
 ##
 ## Code courtesy of [url=https://yosoyfreeman.github.io/article/godot/tutorial/achieving-better-mouse-input-in-godot-4-the-perfect-camera-controller/]Yo Soy Freeman[/url]. Edited by Jeliciousz
 
-const RADIANS_PER_UNIT: float = deg_to_rad(0.075)
+const RADIANS_PER_UNIT: float = deg_to_rad(0.1)
 
 ## Settings.
 @export_group("Settings")
