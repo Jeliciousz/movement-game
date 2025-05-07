61aa7a575a53df937f02cc3b5eb379c5550a1571
Jeli <105231781+Jeliciousz@users.noreply.github.com>
2025-05-07 12:13:31 -0400

    Gain speed while sliding down slopes, lose speed while sliding up slopes

--- scripts/player/states/player_sliding_state.gd
+++ scripts/player/states/player_sliding_state.gd
@@ -7,12 +7,14 @@ extends State
 
 
 func _state_enter() -> void:
+	_player.floor_constant_speed = false
 	shared_vars[&"slide_timestamp"] = Time.get_ticks_msec()
 	shared_vars[&"coyote_slide_active"] = false
 	_player.slide_audio.play()
 
 
 func _state_exit() -> void:
+	_player.floor_constant_speed = true
 	shared_vars[&"slide_timestamp"] = Time.get_ticks_msec()
 	_player.velocity -= _player.velocity.normalized() * _player.slide_stop_force
 
@@ -34,8 +36,8 @@ func _state_physics_preprocess(_delta: float) -> void:
 		return
 
 
-func _state_physics_process(_delta: float) -> void:
-	update_physics()
+func _state_physics_process(delta: float) -> void:
+	update_physics(delta)
 	_player.update()
 
 	if not _player.is_on_floor():
@@ -48,7 +50,10 @@ func _state_physics_process(_delta: float) -> void:
 		return
 
 
-func update_physics() -> void:
+func update_physics(delta: float) -> void:
+	var weight: float = _player.get_floor_normal().dot(_player.velocity.normalized())
+
+	_player.velocity += _player.velocity.normalized() * weight * 15.0 * delta
 	_player.add_air_resistence(_player.physics_air_resistence)
 	_player.add_friction(_player.physics_friction * _player.slide_friction_multiplier, 0)
 	_player.add_movement(0, _player.slide_acceleration)
