12b95babd06662e53fc6c3054f0215ee9bb372bb
Jeli <105231781+Jeliciousz@users.noreply.github.com>
2025-05-16 17:07:15 -0400

    Update kill area

--- scripts/kill_area.gd
+++ scripts/kill_area.gd
@@ -3,11 +3,10 @@ extends Area3D
 ## If an entity with a health component enters this area, they will die.
 
 
-func _physics_process(_delta: float) -> void:
-	for body: PhysicsBody3D in get_overlapping_bodies():
-		if not body.has_node(^"HealthComponent"):
-			continue
+func _on_body_entered(body: PhysicsBody3D) -> void:
+	if not body.has_node(^"HealthComponent"):
+		return
 
-		var health_component: HealthComponent = body.get_node(^"HealthComponent")
+	var health_component: HealthComponent = body.get_node(^"HealthComponent")
 
-		health_component.kill()
+	health_component.kill()
