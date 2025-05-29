2448ad40d2ede11e6fad34ae25fc0cb12dced963
Jeli <105231781+Jeliciousz@users.noreply.github.com>
2025-05-29 18:25:07 -0400

    Reorder variables

--- scripts/components/hitbox_component.gd
+++ scripts/components/hitbox_component.gd
@@ -4,9 +4,9 @@ extends Area3D
 
 ## Should the hitbox kill entities immediately.
 @export var kills: bool = false
-## How much damage the hitbox deals.
-@export var damage: float = 1.0
 ## Should the hitbox only hit once when it overlaps a hurtbox.
 @export var hit_once: bool = true
+## How much damage the hitbox deals.
+@export var damage: float = 1.0
 ## The interval in milliseconds between damage ticks.
 @export var damage_interval: int = 500
