fc9c7024947ba8d43c796b8b93afac7fcb07632c
Jeli <105231781+Jeliciousz@users.noreply.github.com>
2025-05-29 16:35:48 -0400

    Add heal method to health component + documentation

--- scripts/health_component.gd
+++ scripts/health_component.gd
@@ -4,6 +4,8 @@ extends Node
 
 ## Emitted when the entity is damaged.
 signal damaged(damage_taken: float, new_health: float)
+## Emitted when the entity is healed.
+signal healed(health_gained: float, new_health: float)
 ## Emitted when the entity dies.
 signal died(damage_taken: float)
 
@@ -14,6 +16,9 @@ signal died(damage_taken: float)
 @onready var current_health: float = max_health
 
 
+## Damage the health component.
+##
+## Returns whether the health component died from the damage dealt.
 func damage(amount: float) -> bool:
 	# If the damage is enough or more than enough to kill the entity, call kill().
 	if amount >= current_health:
@@ -26,6 +31,19 @@ func damage(amount: float) -> bool:
 	return false
 
 
+## Heal the health component.
+func heal(amount: float, ignore_max_health: bool) -> void:
+	if not ignore_max_health and current_health + amount >= max_health:
+		var health_gained: float = max_health - current_health
+		current_health = max_health
+		healed.emit(health_gained, current_health)
+		return
+
+	current_health += amount
+	healed.emit(amount, current_health)
+
+
+## Kill the health component.
 func kill() -> void:
 	var damage_taken: float = current_health
 
