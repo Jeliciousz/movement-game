d1c0f7336a666c3d22678455d93e83b2b9b90b3d
Jeliciousz <jeliciousz@proton.me>
2025-09-18 09:21:21 -0400

    Remove viewmodel

--- scripts/player/viewmodel/viewmodel_camera.gd
+++
@@ -1,8 +0,0 @@
-extends Camera3D
-
-## The world camera to transform the viewmodel camera to.
-@export var _world_camera: Camera3D
-
-
-func _process(_delta: float) -> void:
-	transform = _world_camera.transform
--- scripts/player/viewmodel/viewmodel_viewport.gd
+++
@@ -1,5 +0,0 @@
-extends SubViewport
-
-
-func _process(_delta: float) -> void:
-	size = get_tree().root.size
