069e12463538d0da344f9af49f2c8dc086247c22
Jeliciousz <jeliciousz@proton.me>
2025-09-18 10:01:23 -0400

    make spedometer lerp instead of move_towards

--- scripts/hud/spedometer.gd
+++ scripts/hud/spedometer.gd
@@ -4,19 +4,18 @@ extends Label
 @export var _player: Player
 
 ## How quickly the speed shown on the spedometeter updates to match the player's speed.
-@export var _speed_track_rate: float = 200.0
+@export var _spedometer_lerp_speed: float = 20.0
 
 var _label_speed: float = 0.0
 
 
 func _process(delta: float) -> void:
-	# Kilometers Per Hour
-	var horizontal_speed: float = _player.get_horizontal_speed() * 3.6
+	var horizontal_speed: float = _player.get_horizontal_speed() * 3.6 # Kilometers Per Hour
 
-	_label_speed = move_toward(_label_speed, horizontal_speed, _speed_track_rate * delta)
+	_label_speed = lerpf(_label_speed, horizontal_speed, _spedometer_lerp_speed * delta)
 
 	text = "%.f kph" % _label_speed
 
 
 func reset_spedometer() -> void:
-	_label_speed = _player.get_horizontal_speed()
+	_label_speed = _player.get_horizontal_speed() * 3.6	# Kilometers Per Hour
