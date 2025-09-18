44fa18ef13824863f84e0182160c28efc149e96c
Jeliciousz <jeliciousz@proton.me>
2025-09-18 09:17:48 -0400

    make spedometer use get_horizontal_speed

--- scripts/hud/spedometer.gd
+++ scripts/hud/spedometer.gd
@@ -11,7 +11,7 @@ var _label_speed: float = 0.0
 
 func _process(delta: float) -> void:
 	# Kilometers Per Hour
-	var horizontal_speed: float = Vector2(_player.velocity.x, _player.velocity.z).length() * 3.6
+	var horizontal_speed: float = _player.get_horizontal_speed() * 3.6
 
 	_label_speed = move_toward(_label_speed, horizontal_speed, _speed_track_rate * delta)
 
