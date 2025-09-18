3caaadfbf3cad45898baa602cbf1ccf5be6c75d0
Jeliciousz <jeliciousz@proton.me>
2025-09-18 09:13:38 -0400

    increase radians per dot

--- scripts/components/mouse_aim_controller.gd
+++ scripts/components/mouse_aim_controller.gd
@@ -5,7 +5,7 @@ extends Node
 ## Code courtesy of [url=https://yosoyfreeman.github.io/article/godot/tutorial/achieving-better-mouse-input-in-godot-4-the-perfect-camera-controller/]Yo Soy Freeman[/url]. Edited by Jeliciousz
 
 ## How many radians should the camera rotate per dot of mouse movement.
-const RADIANS_PER_DOT: float = deg_to_rad(0.07)
+const RADIANS_PER_DOT: float = deg_to_rad(0.1)
 
 ## Settings.
 @export_group("Settings")
