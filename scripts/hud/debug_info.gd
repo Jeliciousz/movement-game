52629170e7743c5e098fd4bd9c6ec8afd55ae711
Jeliciousz <jeliciousz@proton.me>
2025-12-25 16:36:42 -0500

    update debug_info

--- scripts/hud/debug_info.gd
+++ scripts/hud/debug_info.gd
@@ -5,4 +5,5 @@ extends Label
 
 
 func _process(_delta: float) -> void:
-	text = "Active state: %s\nActive stance: %s\nCoyote jump active: %s\nCoyote slide active: %s\nCoyote wall-jump active: %s\nWall-jumps: %s" % [_player.state_machine.get_state_name(), _player.get_stance_as_text(), _player.state_machine.get_shared_var(&"coyote_jump_active"), _player.state_machine.get_shared_var(&"coyote_slide_active"), _player.state_machine.get_shared_var(&"coyote_walljump_active"), _player.state_machine.get_shared_var(&"wall_jumps")]
+	text = "Active state: %s" \
+	+ "Active stance: %s" % [_player.state_machine.get_state_name(), _player.get_stance_as_text()]
