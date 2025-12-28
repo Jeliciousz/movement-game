af883744ca228e6f0ac88a40afe11673975a14bb
Jeliciousz <jeliciousz@proton.me>
2025-12-28 04:58:30 -0500

    Add main menu

--- scripts/global/global.gd
+++ scripts/global/global.gd
@@ -9,21 +9,13 @@ var time: float = 0.0
 
 func _init() -> void:
 	Input.use_accumulated_input = false
-	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	Input.mouse_mode = Input.MOUSE_MODE_CONFINED
 
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventKey:
 		if event.is_action(&"ui_cancel") and event.pressed and not event.echo:
-			if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-				Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-			else:
-				get_tree().quit()
-
-	elif event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			if Input.mouse_mode != Input.MOUSE_MODE_CAPTURED:
-				Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			get_tree().quit()
 
 
 func _physics_process(delta: float) -> void:
