00245b46ee9911894883daba5979f7566a7f98b9
Jeliciousz <jeliciousz@proton.me>
2025-12-28 02:21:41 -0500

    pickup component needs these signals

--- scripts/components/pickup_component.gd
+++ scripts/components/pickup_component.gd
@@ -2,6 +2,9 @@ class_name PickupComponent
 extends Area3D
 ## A component for objects that can be picked-up.
 
+signal picked_up()
+signal dropped()
+
 @export var enabled: bool = true
 
 var _is_picked_up: bool = false
