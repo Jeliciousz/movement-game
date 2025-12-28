667837e2dfdf28d6838ba3a9d5a61b1436d98946
Jeliciousz <jeliciousz@proton.me>
2025-12-28 06:15:35 -0500

    remove play button script

--- scripts/ui/play_button.gd
+++
@@ -1,8 +0,0 @@
-extends Button
-
-var game_scene: PackedScene = preload("res://scenes/levels/test.tscn")
-
-
-func _on_pressed() -> void:
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	get_tree().change_scene_to_packed(game_scene)
